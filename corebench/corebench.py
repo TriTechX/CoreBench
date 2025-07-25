54b9c25c3e2916968a8231c4785d400b57426ce6
TriTech <136884744+TriTechX@users.noreply.github.com>
2025-07-25 19:20:11 +0100

    Update corebench.py

--- corebench/corebench.py
+++ corebench/corebench.py
@@ -307,7 +307,7 @@ def getData():
             quit()
             
         #UPDATE THIS WITH EVERY VERSION
-        version = "1.5.0"
+        version = "1.5.1"
         #UPDATE THIS WITH EVERY VERSION
         
         endLoad = True
